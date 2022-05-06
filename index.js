996ba25ef8172c3116b9f79886386206770e31b5
Md-Nihal <mdnihal01716@gmail.com>
2022-05-06 15:41:23 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+/*
+https://github.com/ProgrammingHeroWC4/warehouse-management-server-side-Md-Nihal
+*/
+
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +14,28 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.spy3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('supplyChain').collection('products')
+
+
+    }
+    catch {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('running the server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('listening from', port)
 })
