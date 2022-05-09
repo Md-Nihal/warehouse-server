8ded0d86df35ee587bf256d8229364a02d724854
Md-Nihal <mdnihal01716@gmail.com>
2022-05-09 20:43:50 +0600

    jwt token added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ https://github.com/ProgrammingHeroWC4/warehouse-management-server-side-Md-Nihal
 
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,32 @@ async function run() {
     try {
         await client.connect();
         const productsCollection = client.db('supplyChain').collection('products')
+
+        function verifyJWT(req, res, next) {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: "Unauthorized access" })
+            }
+            const token = authHeader.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(403).send({ message: "forbidden access" })
+                }
+                console.log(decoded)
+                req.decoded = decoded;
+                next();
+            })
+
+        }
+
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken });
+        })
 
         app.get('/products', async (req, res) => {
             const query = {};
