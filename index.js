391bc2a6872a8a288aab752270af0d4a809efb1d
Md-Nihal <mdnihal01716@gmail.com>
2022-05-09 17:54:09 +0600

    update quantity

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ https://github.com/ProgrammingHeroWC4/warehouse-management-server-side-Md-Nihal
 
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectID } = require('bson');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -33,9 +32,36 @@ async function run() {
 
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectID(id) };
+            const query = { _id: ObjectId(id) };
             const products = await productsCollection.findOne(query);
             res.send(products);
+        });
+
+        app.post('/products', async (req, res) => {
+            const newproduct = req.body;
+            const result = await productsCollection.insertOne(newproduct);
+            res.send(result);
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        app.put('/added/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    quantity: updateQuantity.newQuantity
+                }
+            }
+            const result = await productsCollection.updateOne(filter, options, updatedDoc)
+            res.send(result)
         })
 
     }
